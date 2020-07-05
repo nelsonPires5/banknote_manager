1432e8b17a97711edba6b437438ea2f66de2be23
Nelson Pires <nelson.pires5@hotmail.com>
2020-07-05 15:15:40 -0300

    adding db initialization tables

--- backend/db.py
+++ backend/db.py
@@ -11,14 +11,24 @@ class Connect():
             self.cursor.execute('SELECT SQLITE_VERSION()')
             self.data = self.cursor.fetchone()
             print("SQLite version: %s" % self.data)
+            self._initialize_db('backend/schemas/clients.sql')
+            self._initialize_db('backend/schemas/bills.sql')
         except sqlite3.Error:
             print("Erro ao abrir banco.")
 
-    def commit_db():
+    def commit_db(self):
+        """Grava no banco de dados"""
         if self.conn:
             self.conn.commit()
 
-    def close_db():
+    def close_db(self):
+        """Fecha conexão com o banco de dados"""
         if self.conn:
             self.conn.close()
             print("Conexão fechada!")
+    
+    def _initialize_db(self, script_path: str):
+        """Roda queries sql de inicialização do db"""
+        with open(script_path, 'rt') as f:
+            schema = f.read()
+            self.cursor.executescript(schema)
