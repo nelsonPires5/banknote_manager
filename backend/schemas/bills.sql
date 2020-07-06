34d02acd05d671b1d042359dd015b0aec0c18f4f
nelson <nelson.pires5@hotmail.com>
2020-07-06 19:55:47 -0300

    deleting schemas because sqlalchemy don't use them

--- backend/schemas/bills.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS bills (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        created_at DATE NOT NULL,
-        cpf_cnpj INTEGER NOT NULL,
-        amount FLOAT NOT NULL,
-        installment INTEGER NOT NULL,
-        amount_installment FLOAT NOT NULL
-);
