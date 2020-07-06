d29cca0bb14eef65fb6ebe5cceb48aac92a4953c
nelson <nelson.pires5@hotmail.com>
2020-07-06 19:55:07 -0300

    testing db module

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from backend.db import Connect
 if __name__ == "__main__":
     db_conn = Connect('primary.db')
     # db_conn.export_db('backup.sql')
+    # db_conn.import_db('backup.sql')
+    # db_conn = Connect('primary.db')
+    # db_conn.export_db('backup.sql')
