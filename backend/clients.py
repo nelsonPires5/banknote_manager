374822d2337cbd630e4a0003d8ba8ea4c93592fd
nelson <nelson.pires5@hotmail.com>
2020-07-08 08:04:04 -0300

    Implementing delete and read_all methods

--- backend/clients.py
+++ backend/clients.py
@@ -1,4 +1,6 @@
 from backend.db import Connect
+from sqlalchemy import update, delete, select
+
 
 class Client():
     """Client main class"""
@@ -17,7 +19,7 @@ class Client():
     ) -> None:
         self.engine = db_connection.engine
         self.clients_table = db_connection.clients_table
-        self.cpf_cnpj = cpf_cpnj
+        self.cpf_cnpj = cpf_cnpj
         self.full_name = full_name
         self.address = address
         self.address_number = address_number
@@ -29,18 +31,38 @@ class Client():
 
     def create(self) -> None:
         """Create client in the database"""
-        self.conn = self.engine.connect()
+        conn = self.engine.connect()
         insert = self.clients_table.insert()
         new_client = insert.values(
-            cpf_cnpj = self.cpf_cnpj,
-            full_name = self.full_name,
-            company_name = self.company_name,
-            address = self.address,
-            address_number = self.address_number,
-            district = self.district,
-            city = self.city,
-            phone_number = self.phone_number,
-            notes = self.notes
+            cpf_cnpj=self.cpf_cnpj,
+            full_name=self.full_name,
+            company_name=self.company_name,
+            address=self.address,
+            address_number=self.address_number,
+            district=self.district,
+            city=self.city,
+            phone_number=self.phone_number,
+            notes=self.notes
         )
-        self.conn.execute(new_client)
-        self.conn.close()
+        conn.execute(new_client)
+        conn.close()
+
+    def read_all(self):
+        """Read client database"""
+        sele = select([self.clients_table])
+        return sele.execute()
+
+    def update(self, cpf_cnpj: int, column_and_change: dict) -> None:
+        """Update an specific client"""
+        conn = self.engine.connect()
+        updt = update(self.clients_table) \
+            .where(self.clients_table.c.cpf_cpnj == cpf_cnpj)
+        # example updt = updt.values(nome='qualquercoisa')
+        conn.execute(updt)
+
+    def delete_client(self, cpf_cnpj: int) -> None:
+        """Delete an specific client"""
+        conn = self.engine.connect()
+        dele = delete(self.clients_table) \
+            .where(self.clients_table.c.cpf_cpnj == cpf_cnpj)
+        conn.execute(dele)
