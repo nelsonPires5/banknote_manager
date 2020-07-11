cbcc831c3aa4e0709a90b43b775adb5d2d2ca5b5
Nelson Pires <nelson.pires5@hotmail.com>
2020-07-11 17:07:51 -0300

    change clients constructor method and create

--- backend/clients.py
+++ backend/clients.py
@@ -6,7 +6,13 @@ class Client():
     """Client main class"""
     def __init__(
         self,
-        db_connection: Connect,
+        db_connection: Connect
+    ) -> None:
+        self.engine = db_connection.engine
+        self.clients_table = db_connection.clients_table
+
+    def create(
+        self,
         cpf_cnpj: int,
         full_name: str,
         address: str,
@@ -17,48 +23,36 @@ class Client():
         phone_number: str = None,
         notes: str = None
     ) -> None:
-        self.engine = db_connection.engine
-        self.clients_table = db_connection.clients_table
-        self.cpf_cnpj = cpf_cnpj
-        self.full_name = full_name
-        self.address = address
-        self.address_number = address_number
-        self.district = district
-        self.city = city
-        self.company_name = company_name
-        self.phone_number = phone_number
-        self.notes = notes
-
-    def create(self) -> None:
         """Create client in the database"""
         conn = self.engine.connect()
         insert = self.clients_table.insert()
         new_client = insert.values(
-            cpf_cnpj=self.cpf_cnpj,
-            full_name=self.full_name,
-            company_name=self.company_name,
-            address=self.address,
-            address_number=self.address_number,
-            district=self.district,
-            city=self.city,
-            phone_number=self.phone_number,
-            notes=self.notes
+            cpf_cnpj=cpf_cnpj,
+            full_name=full_name,
+            company_name=company_name,
+            address=address,
+            address_number=address_number,
+            district=district,
+            city=city,
+            phone_number=phone_number,
+            notes=notes
         )
         conn.execute(new_client)
         conn.close()
 
-    def read_all(self):
+    def read_all(self) -> list:
         """Read client database"""
         sele = select([self.clients_table])
-        return sele.execute()
+        return [row for row in sele.execute()] 
 
     def update(self, cpf_cnpj: int, column_and_change: dict) -> None:
         """Update an specific client"""
         conn = self.engine.connect()
         updt = update(self.clients_table) \
-            .where(self.clients_table.c.cpf_cpnj == cpf_cnpj)
-        # example updt = updt.values(nome='qualquercoisa')
+            .where(self.clients_table.c.cpf_cpnj == cpf_cnpj) \
+            .values(column_and_change)
         conn.execute(updt)
+        conn.close()
 
     def delete_client(self, cpf_cnpj: int) -> None:
         """Delete an specific client"""
@@ -66,3 +60,4 @@ class Client():
         dele = delete(self.clients_table) \
             .where(self.clients_table.c.cpf_cpnj == cpf_cnpj)
         conn.execute(dele)
+        conn.close()
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from backend.db import Connect
+from backend.clients import Client
 
 if __name__ == "__main__":
     db_conn = Connect('primary.db')
+    cl = Client(db_conn)
+    # cl.create(        
+    #     22689495618, # Tem que ser string pq não permite começar com zero
+    #     'Nelson Pires',
+    #     'Rua cristiano viana',
+    #     '605',
+    #     'Pinheiros',
+    #     'São Paulo'
+    # )
+    print(cl.read_all())
     # db_conn.export_db('backup.sql')
     # db_conn.import_db('backup.sql')
     # db_conn = Connect('primary.db')
